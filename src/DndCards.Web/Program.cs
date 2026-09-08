a136416d5634271def4db23cf259aa29cbd89189
Dominic Ammann <dominic.ammann@giag.ch>
2026-09-08 15:25:57 +0200

    feat: scaffold Aspire AppHost and Blazor Web App project

---
+++ src/DndCards.Web/Program.cs
@@ -0,0 +1,38 @@
+using DndCards.Web.Components;
+using DndCards.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.Configure<UsageLimitOptions>(builder.Configuration.GetSection("Monetization"));
+builder.Services.AddSingleton<UsageLimitService>();
+builder.Services.AddSingleton<CardHtmlBuilder>();
+builder.Services.AddSingleton<PdfCardService>();
+builder.Services.AddSingleton<DndBeyondImportService>();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
